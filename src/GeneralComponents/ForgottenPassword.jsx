4f98057de9b39cda8f13cea384ce3c47c3e6cfdf
palmergk <palmergid@gmail.com>
2024-09-11 23:32:26 +0100

    updates

--- src/GeneralComponents/ForgottenPassword.jsx
+++ src/GeneralComponents/ForgottenPassword.jsx
@@ -150,99 +150,101 @@ const ForgottenPassword = ({ closeView }) => {
 
     return (
         <ModalLayout toggler={toggler} closeView={closeView}>
-            <div className='bg-white py-4 w-fit h-fit rounded-xl shld overflow-auto relative' ref={toggler}>
-                {loading && <Loading />}
-                {screen === 1 && <>
-                    <div className='md:w-[85%] w-11/12 mx-auto'>
-                        <form onSubmit={FindEmail}>
-                            <div className='flex justify-center flex-col gap-2 items-center'>
-                                <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
-                                    <IoLockClosedOutline className='text-2xl' />
+            <div className='px-4'>
+                <div className='bg-white py-4 w-fit h-fit rounded-xl shld overflow-auto relative' ref={toggler}>
+                    {loading && <Loading />}
+                    {screen === 1 && <>
+                        <div className='md:w-[85%] w-11/12 mx-auto'>
+                            <form onSubmit={FindEmail}>
+                                <div className='flex justify-center flex-col gap-2 items-center'>
+                                    <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
+                                        <IoLockClosedOutline className='text-2xl' />
+                                    </div>
+                                    <div className='text-[0.9rem] font-extrabold'>Trouble logging in?</div>
+                                    <div className='text-center text-[0.8rem] font-[600]'>Enter your email address to find your account and reset password</div>
                                 </div>
-                                <div className='text-[0.9rem] font-extrabold'>Trouble logging in?</div>
-                                <div className='text-center text-[0.8rem] font-[600]'>Enter your email address to find your account and reset password</div>
-                            </div>
-                            <div className='flex flex-col gap-5 mt-8'>
-                                <div className='flex flex-col gap-2 relative'>
-                                    <div className='text-xs capitalize font-[600]'>enter email address</div>
-                                    <input className='outline-none w-full border-b border-black lg:text-[0.9rem] text-base ipt input-off' type='email' placeholder='E.g: john14@gmail.com' name='email' value={form.email} onChange={formHandler}></input>
-                                    {error === 'email' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                <div className='flex flex-col gap-5 mt-8'>
+                                    <div className='flex flex-col gap-2 relative'>
+                                        <div className='text-xs capitalize font-[600]'>enter email address</div>
+                                        <input className='outline-none w-full border-b border-black lg:text-[0.9rem] text-base ipt input-off' type='email' placeholder='E.g: john14@gmail.com' name='email' value={form.email} onChange={formHandler}></input>
+                                        {error === 'email' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                    </div>
+                                    <div className='flex items-center justify-center mt-2'>
+                                        <button className='outline-none bg-orange py-2 md:px-24 h-fit md:w-fit w-full rounded-md capitalize text-[0.9rem] text-white cursor-pointer font-[550]' >find account</button>
+                                    </div>
                                 </div>
-                                <div className='flex items-center justify-center mt-2'>
-                                    <button className='outline-none bg-orange py-2 md:px-24 h-fit md:w-fit w-full rounded-md capitalize text-[0.9rem] text-white cursor-pointer font-[550]' >find account</button>
+                            </form>
+                        </div>
+                    </>}
+                    {screen === 2 && <>
+                        <div className='md:w-[85%] w-11/12 mx-auto'>
+                            <form onSubmit={VerifyEmail}>
+                                <div className='flex justify-center flex-col gap-2 items-center'>
+                                    <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
+                                        <MdVerified className='text-[1.7rem]' />
+                                    </div>
+                                    <div className='text-[0.9rem] font-extrabold'>Verify your email address</div>
+                                    <div className='text-center text-[0.8rem] font-[600]'>A verification code was sent to your email address, copy and paste the code below</div>
                                 </div>
-                            </div>
-                        </form>
-                    </div>
-                </>}
-                {screen === 2 && <>
-                    <div className='md:w-[85%] w-11/12 mx-auto'>
-                        <form onSubmit={VerifyEmail}>
-                            <div className='flex justify-center flex-col gap-2 items-center'>
-                                <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
-                                    <MdVerified className='text-[1.7rem]' />
+                                <div className='flex flex-col gap-5 mt-8'>
+                                    <div className='flex flex-col gap-2 relative'>
+                                        <div className='text-xs capitalize font-[600]'>enter verification code:</div>
+                                        <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off ipt' type='text' placeholder='Six digits code' name='code' value={form.code} onChange={formHandler}></input>
+                                        {error === 'code' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                    </div>
+                                    <div className='flex items-center justify-center mt-2'>
+                                        <button className='outline-none bg-orange py-2 md:px-[7.5rem] h-fit md:w-fit w-full rounded-md capitalize text-[0.9rem] text-white cursor-pointer font-[550]' >verify email</button>
+                                    </div>
                                 </div>
-                                <div className='text-[0.9rem] font-extrabold'>Verify your email address</div>
-                                <div className='text-center text-[0.8rem] font-[600]'>A verification code was sent to your email address, copy and paste the code below</div>
-                            </div>
-                            <div className='flex flex-col gap-5 mt-8'>
-                                <div className='flex flex-col gap-2 relative'>
-                                    <div className='text-xs capitalize font-[600]'>enter verification code:</div>
-                                    <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off ipt' type='text' placeholder='Six digits code' name='code' value={form.code} onChange={formHandler}></input>
-                                    {error === 'code' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                            </form>
+                        </div>
+                    </>}
+                    {screen === 3 && <>
+                        <div className='md:w-[85%] w-11/12 mx-auto'>
+                            <form onSubmit={ChangePassword}>
+                                <div className='flex justify-center flex-col gap-2 items-center'>
+                                    <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
+                                        <IoLockOpenOutline className='text-2xl' />
+                                    </div>
+                                    <div className='text-[0.9rem] font-extrabold capitalize'>password re-set</div>
+                                    <div className='text-center text-[0.8rem] font-[600]'>Set a new password for your account by filling the password fields below</div>
                                 </div>
-                                <div className='flex items-center justify-center mt-2'>
-                                    <button className='outline-none bg-orange py-2 md:px-[7.5rem] h-fit md:w-fit w-full rounded-md capitalize text-[0.9rem] text-white cursor-pointer font-[550]' >verify email</button>
+                                <div className='flex flex-col gap-5 mt-8'>
+                                    <div className='flex flex-col gap-2 relative'>
+                                        <div className='text-xs capitalize font-[600]'>enter new password</div>
+                                        <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off  ipt' type={eye === true ? 'text' : 'password'} placeholder='Characters more than five' name='new_password' value={form.new_password} onChange={formHandler}></input>
+                                        <EyeIcon className='absolute bottom-0 right-0 text-base text-orange cursor-pointer' onClick={() => setEye(!eye)} />
+                                        {error === 'password' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                    </div>
+                                    <div className='flex flex-col gap-2 relative'>
+                                        <div className='text-xs capitalize font-[600]'>confirm password</div>
+                                        <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off ipt' type={eye2 === true ? 'text' : 'password'} placeholder='Re-type password' name='confirm_password' value={form.confirm_password} onChange={formHandler}></input>
+                                        <EyeIcon2 className='absolute bottom-0 right-0 text-base text-orange cursor-pointer' onClick={() => setEye2(!eye2)} />
+                                        {error === 'confirm_p' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                    </div>
+                                    <div className='flex items-center justify-center mt-2'>
+                                        <button className='outline-none bg-orange py-2 md:px-24 h-fit ,md:w-fit w-full rounded-md capitalize text-[0.8rem] text-white cursor-pointer font-[550]'>change password</button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
-                    </div>
-                </>}
-                {screen === 3 && <>
-                    <div className='md:w-[85%] w-11/12 mx-auto'>
-                        <form onSubmit={ChangePassword}>
-                            <div className='flex justify-center flex-col gap-2 items-center'>
-                                <div className='w-12 h-12 border-2 border-black rounded-full flex items-center justify-center'>
-                                    <IoLockOpenOutline className='text-2xl' />
+                            </form>
+                        </div>
+                    </>}
+                    {screen === 4 && <>
+                        <div className='md:w-[85%] w-11/12 mx-auto'>
+                            <div className='flex flex-col gap-2 justify-center items-center'>
+                                <div className='w-12 h-12 border-2 border-[green] rounded-full flex items-center justify-center'>
+                                    <RiVerifiedBadgeLine className='text-2xl text-[green]' />
                                 </div>
-                                <div className='text-[0.9rem] font-extrabold capitalize'>password re-set</div>
-                                <div className='text-center text-[0.8rem] font-[600]'>Set a new password for your account by filling the password fields below</div>
-                            </div>
-                            <div className='flex flex-col gap-5 mt-8'>
-                                <div className='flex flex-col gap-2 relative'>
-                                    <div className='text-xs capitalize font-[600]'>enter new password</div>
-                                    <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off  ipt' type={eye === true ? 'text' : 'password'} placeholder='Characters more than five' name='new_password' value={form.new_password} onChange={formHandler}></input>
-                                    <EyeIcon className='absolute bottom-0 right-0 text-base text-orange cursor-pointer' onClick={() => setEye(!eye)} />
-                                    {error === 'password' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
+                                <div className='text-[0.9rem] font-extrabold uppercase text-center'>password Reset!</div>
+                                <div className='text-center text-[0.8rem] font-[600]'>Password reset successful, you can now login with new password created</div>
+                                <div className='flex gap-1 cursor-pointer mt-4 items-center text-sm text-[green] hover:text-orange' onClick={closeView}>
+                                    <div className='font-[600]'>Back to login</div>
+                                    <FaArrowRight />
                                 </div>
-                                <div className='flex flex-col gap-2 relative'>
-                                    <div className='text-xs capitalize font-[600]'>confirm password</div>
-                                    <input className='outline-none w-full  border-b border-black lg:text-[0.9rem] text-base input-off ipt' type={eye2 === true ? 'text' : 'password'} placeholder='Re-type password' name='confirm_password' value={form.confirm_password} onChange={formHandler}></input>
-                                    <EyeIcon2 className='absolute bottom-0 right-0 text-base text-orange cursor-pointer' onClick={() => setEye2(!eye2)} />
-                                    {error === 'confirm_p' && <div className='text-xs text-[red] absolute -bottom-4 left-0'>{errorMsg}</div>}
-                                </div>
-                                <div className='flex items-center justify-center mt-2'>
-                                    <button className='outline-none bg-orange py-2 md:px-24 h-fit ,md:w-fit w-full rounded-md capitalize text-[0.8rem] text-white cursor-pointer font-[550]'>change password</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </>}
-                {screen === 4 && <>
-                    <div className='md:w-[85%] w-11/12 mx-auto'>
-                        <div className='flex flex-col gap-2 justify-center items-center'>
-                            <div className='w-12 h-12 border-2 border-[green] rounded-full flex items-center justify-center'>
-                                <RiVerifiedBadgeLine className='text-2xl text-[green]' />
-                            </div>
-                            <div className='text-[0.9rem] font-extrabold uppercase text-center'>password Reset!</div>
-                            <div className='text-center text-[0.8rem] font-[600]'>Password reset successful, you can now login with new password created</div>
-                            <div className='flex gap-1 cursor-pointer mt-4 items-center text-sm text-[green] hover:text-orange' onClick={closeView}>
-                                <div className='font-[600]'>Back to login</div>
-                                <FaArrowRight />
                             </div>
                         </div>
-                    </div>
-                </>}
+                    </>}
+                </div>
             </div>
         </ModalLayout>
     )
